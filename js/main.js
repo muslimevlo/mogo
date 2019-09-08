c84ac9e10a16f51136c0583b09a4d460cce1c027
muslimevlo <muslimevlo@yandex.ru>
2019-09-08 12:56:54 +0300

    делаем слайдер

--- js/main.js
+++ js/main.js
@@ -7,4 +7,11 @@ $( document ).ready(function() {
 
         $this.toggleClass("active");
     });
+    
+    //slick slider initial start
+    $('#review-slider').slick({
+        nextArrow: '<span class="slider-next-btn slider-nav-btn"><img src="images/icons/arrow-next.svg"></span>',
+        prevArrow: '<span class="slider-prev-btn slider-nav-btn"><img src="images/icons/arrow-prev.svg"></span>',
+    });
+    //slick slider initial end
 });
