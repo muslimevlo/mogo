7eb6bf292fea5ef50864d160c8bdb475fa0510cb
muslimevlo <muslimevlo@yandex.ru>
2019-09-09 10:38:42 +0300

    LATEST BLOG

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $( document ).ready(function() {
     });
     
     //slick slider initial start
-    $('#review-slider').slick({
+    $('.review-slider').slick({
         nextArrow: '<span class="slider-next-btn slider-nav-btn"><img src="images/icons/arrow-next.svg"></span>',
         prevArrow: '<span class="slider-prev-btn slider-nav-btn"><img src="images/icons/arrow-prev.svg"></span>',
     });
